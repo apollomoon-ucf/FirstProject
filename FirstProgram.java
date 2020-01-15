55165107d1c702f01cdb61b94bad24d756684e2b
Brian Moon <brianmoon@MacBook-Pro.local>
2020-01-14 19:13:52 -0500

    first commit

---
+++ FirstProgram.java
@@ -0,0 +1,34 @@
+
+
+public class FirstProgram
+{
+  public static void main(String[] args)
+  {
+
+    // declaring vars
+    int age = 13;
+    double weight = 123.345678;
+
+    // testing prints
+    // printf allows you to print out vars
+    System.out.printf("Hello everyone! I am %d years old! I weigh %.2f\n", age, weight);
+    // println automatically prints a new line after the statement
+    System.out.println("Hello everyone!");
+    // print requires a new line char at the end just like C
+    System.out.print("Hello again everyone!\n");
+
+    // basic arithmetic
+    System.out.println(10 + 12);
+    System.out.println(10 / 12);
+    System.out.println(10.0 / 12.0);
+
+    int x = 12;
+    int y = 10;
+    double u = 10.0;
+    System.out.println(x / 0.001);
+    System.out.println(y / u);
+    System.out.println(x % u);
+    System.out.println(10.0 % 12.0); // you can use mod arith on floating point in java even though you really shouldnt
+
+  }
+}
